2b70f7f75540bc07cd33e628db769a08aa6780d0
ctyo <ctyo@live.jp>
2020-07-06 09:13:08 +0900

    fix prod path

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: './static',
-    assetsPublicPath: '/clientside_thumbmap/',
+    assetsPublicPath: '/',
 
     /**
      * Source Maps
